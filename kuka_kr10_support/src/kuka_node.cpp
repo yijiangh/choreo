9045a3b2fd5c791ed0a987e90ad6275fb324b0e8
cookt <tomcook@mit.edu>
2017-03-22 19:06:15 -0700

    redesign nodes to interact

--- kuka_kr10_support/src/kuka_node.cpp
+++ kuka_kr10_support/src/kuka_node.cpp
@@ -1,97 +1,16 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2013, SRI International
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of SRI International nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-/* Author: Sachin Chitta */
-
 #include <moveit/move_group_interface/move_group.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
-
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
-
 #include <moveit_msgs/AttachedCollisionObject.h>
 #include <moveit_msgs/CollisionObject.h>
+#include <geometry_msgs/PoseArray.h>
+#include <ros/ros.h>
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "kuka_node");
-  ros::NodeHandle node_handle;  
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+static moveit::planning_interface::MoveGroup group("manipulator");
+static ros::Publisher display_publisher;
 
-
-  /* This sleep is ONLY to allow Rviz to come up */
-  sleep(20.0);
-  
-  // BEGIN_TUTORIAL
-  // 
-  // Setup
-  // ^^^^^
-  // 
-  // The :move_group_interface:`MoveGroup` class can be easily 
-  // setup using just the name
-  // of the group you would like to control and plan for.
-  moveit::planning_interface::MoveGroup group("manipulator");
-
-  // We will use the :planning_scene_interface:`PlanningSceneInterface`
-  // class to deal directly with the world.
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;  
-
-  // (Optional) Create a publisher for visualizing plans in Rviz.
-  ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-  moveit_msgs::DisplayTrajectory display_trajectory;
-
-  // Getting Basic Information
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // We can print the name of the reference frame for this robot.
-  ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
-  
-  // We can also print the name of the end-effector link for this group.
-  ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
-
-  // Planning to a Pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^
-  // We can plan a motion for this group to a desired pose for the 
-  // end-effector.
-  geometry_msgs::Pose target_pose1;
-  target_pose1.orientation.w = 1.0;
-  target_pose1.position.x = 0.28;
-  target_pose1.position.y = -0.7;
-  target_pose1.position.z = 1.0;
-  group.setPoseTarget(target_pose1);
-
+void frameCallback(geometry_msgs::PoseArray msg){
 
   // Now, we call the planner to compute the plan
   // and visualize it.
@@ -103,7 +22,8 @@ int main(int argc, char **argv)
   ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");    
   /* Sleep to give Rviz time to visualize the plan. */
   sleep(5.0);
-
+  
+  moveit_msgs::DisplayTrajectory display_trajectory;
   // Visualizing plans
   // ^^^^^^^^^^^^^^^^^
   // Now that we have a plan we can visualize it in Rviz.  This is not
@@ -120,103 +40,33 @@ int main(int argc, char **argv)
     sleep(5.0);
   }
   
-  // Moving to a pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Moving to a pose goal is similar to the step above
-  // except we now use the move() function. Note that
-  // the pose goal we had set earlier is still active 
-  // and so the robot will try to move to that goal. We will
-  // not use that function in this tutorial since it is 
-  // a blocking function and requires a controller to be active
-  // and report success on execution of a trajectory.
- 
-  /* Uncomment below line when working with a real robot*/
-  /* group.move() */
+}
 
-  // Planning to a joint-space goal 
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "kuka_node");
+  ros::NodeHandle node_handle;  
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  ros::Subscriber frame_sub = node_handle.subscribe("framelinks", 0 , frameCallback);
+
+  /* This sleep is ONLY to allow Rviz to come up */
+  sleep(20.0);
+  // We will use the :planning_scene_interface:`PlanningSceneInterface`
+  // class to deal directly with the world.
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;  
+
+  // Publisher for visualizing plans in Rviz.
+  display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+
+  // Getting Basic Information
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^
   //
-  // Let's set a joint space goal and move towards it.  This will replace the
-  // pose target we set above.
-  //
-  // First get the current set of joint values for the group.
-  std::vector<double> group_variable_values;
-  group.getCurrentState()->copyJointGroupPositions(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()), group_variable_values);
+  // We can print the name of the reference frame for this robot.
+  ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
   
-  // Now, let's modify one of the joints, plan to the new joint
-  // space goal and visualize the plan.
-  group_variable_values[0] = -1.0;  
-  group.setJointValueTarget(group_variable_values);
-  success = group.plan(my_plan);
-
-  ROS_INFO("Visualizing plan 2 (joint space goal) %s",success?"":"FAILED");
-  /* Sleep to give Rviz time to visualize the plan. */
-  sleep(5.0);
-
-  // We will reuse the old goal that we had and plan to it.
-  // Note that this will only work if the current state already 
-  // satisfies the path constraints. So, we need to set the start
-  // state to a new pose. 
-  robot_state::RobotState start_state(*group.getCurrentState());
-  geometry_msgs::Pose start_pose2;
-  start_pose2.orientation.w = 1.0;
-  start_pose2.position.x = 0.55;
-  start_pose2.position.y = -0.05;
-  start_pose2.position.z = 0.8;
-  const robot_state::JointModelGroup *joint_model_group =
-                  start_state.getJointModelGroup(group.getName());
-  start_state.setFromIK(joint_model_group, start_pose2);
-  group.setStartState(start_state);
-  
-  // Now we will plan to the earlier pose target from the new 
-  // start state that we have just created.
-  group.setPoseTarget(target_pose1);
-  success = group.plan(my_plan);
-
-  ROS_INFO("Visualizing plan 3 (constraints) %s",success?"":"FAILED");
-  /* Sleep to give Rviz time to visualize the plan. */
-  sleep(10.0);
-
-  // When done with the path constraint be sure to clear it.
-  group.clearPathConstraints();
-
-  // Cartesian Paths
-  // ^^^^^^^^^^^^^^^
-  // You can plan a cartesian path directly by specifying a list of waypoints 
-  // for the end-effector to go through. Note that we are starting 
-  // from the new start state above.  The initial pose (start state) does not
-  // need to be added to the waypoint list.
-  std::vector<geometry_msgs::Pose> waypoints;
-
-  geometry_msgs::Pose target_pose3 = start_pose2;
-  target_pose3.position.x += 0.2;
-  target_pose3.position.z += 0.2;
-  waypoints.push_back(target_pose3);  // up and out
-
-  target_pose3.position.y -= 0.2;
-  waypoints.push_back(target_pose3);  // left
-
-  target_pose3.position.z -= 0.2;
-  target_pose3.position.y += 0.2;
-  target_pose3.position.x -= 0.2;
-  waypoints.push_back(target_pose3);  // down and right (back to start)
-
-  // We want the cartesian path to be interpolated at a resolution of 1 cm
-  // which is why we will specify 0.01 as the max step in cartesian
-  // translation.  We will specify the jump threshold as 0.0, effectively
-  // disabling it.
-  moveit_msgs::RobotTrajectory trajectory;
-  double fraction = group.computeCartesianPath(waypoints,
-                                               0.01,  // eef_step
-                                               0.0,   // jump_threshold
-                                               trajectory);
-
-  ROS_INFO("Visualizing plan 4 (cartesian path) (%.2f%% acheived)",
-        fraction * 100.0);    
-  /* Sleep to give Rviz time to visualize the plan. */
-  sleep(15.0);
-
+  // We can also print the name of the end-effector link for this group.
+  ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
 
   ros::shutdown();  
   return 0;
--- kuka_kr10_support/src/teleop_panel.cpp
+++ kuka_kr10_support/src/teleop_panel.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <utility>
 #include <QFile>
 #include <QPainter>
-#include <QLineEdit>
+#include <QTextStream>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QTimer>
 #include <QFileDialog>
 #include <QTextEdit>
-#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseArray.h>
 #include "teleop_panel.h"
 
 namespace framefab 
 {
+
 TeleopPanel::TeleopPanel( QWidget* parent )
   : rviz::Panel( parent )
 {
@@ -30,7 +32,6 @@ TeleopPanel::TeleopPanel( QWidget* parent )
   layout->addLayout( topic_layout );
   layout->addWidget( file_display_ );
   setLayout( layout );
-
   // Create a timer for sending the output.  Motor controllers want to
   // be reassured frequently that they are doing the right thing, so
   // we keep re-sending velocities even when they aren't changing.
@@ -44,7 +45,8 @@ TeleopPanel::TeleopPanel( QWidget* parent )
 
   // Next we make signal/slot connections.
   connect( button_ , SIGNAL( clicked() ), this, SLOT( readFile() ));
-
+  
+  pose_publisher_ = nh_.advertise<geometry_msgs::PoseArray>("/framelinks",1000);
   // Start the timer.
   output_timer->start( 100 );
 
@@ -52,20 +54,44 @@ TeleopPanel::TeleopPanel( QWidget* parent )
 
 
 
-// Read the topic name from the QLineEdit and call setTopic() with the
-// results.  This is connected to QLineEdit::editingFinished() which
-// fires when the user presses Enter or Tab or otherwise moves focus
-// away.
+// Read the file name from the dialog and parse results
 void TeleopPanel::readFile()
 {
+    //set up graph rep
+    std::vector<geometry_msgs::Point> nodes;
+    std::vector<std::pair<int,int> > edges;
+    std::vector<std::pair<int,int> > pillars;
+    std::vector<std::pair<int,int> > ceilings;
+    //open file
     QString fileName = QFileDialog::getOpenFileName(this,
-         tr("Open File 1"), "/home", tr("txt Files (*.txt)"));
+         tr("Open File"), "/home/ubuntu", tr("txt Files (*.pwf)"));
     QFile inputFile(fileName);
-    if (!inputFile.open(QIODevice::ReadOnly | QIODevice::Text))
-	return;
-    QString contents = inputFile.readAll();
-    file_display_->setText(contents);
-    seq_file_edit_->setText(fileName);
+    if (inputFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&inputFile);
+	while(!in.atEnd())
+	{
+	  QString line = in.readLine();
+	  QStringList tokens = line.split(" ");
+	  if(tokens.at(0) == "v") {
+	     geometry_msgs::Point p;
+	     p.x = tokens.at(1).toFloat();
+   	     p.y = tokens.at(2).toFloat();
+	     p.z = tokens.at(3).toFloat();
+ 	     nodes.push_back(p);
+	  } else if (tokens.at(0) == "p") { 
+	     pillars.push_back(std::make_pair(tokens.at(1).toInt(), tokens.at(2).toInt()));
+	     edges.push_back(std::make_pair(tokens.at(1).toInt(), tokens.at(2).toInt()));
+	  } else if (tokens.at(0) == "l") {
+             edges.push_back(std::make_pair(tokens.at(1).toInt(), tokens.at(2).toInt()));
+	  } else if (tokens.at(0) =="c") {
+	     ceilings.push_back(std::make_pair(tokens.at(1).toInt(), tokens.at(2).toInt()));
+	     edges.push_back(std::make_pair(tokens.at(1).toInt(), tokens.at(2).toInt()));
+	  }
+	}
+	inputFile.close();
+    }
+    seq_file_editor_->setText(fileName);
     // rviz::Panel defines the configChanged() signal.  Emitting it
     // tells RViz that something in this panel has changed that will
     // affect a saved config file.  Ultimately this signal can cause
