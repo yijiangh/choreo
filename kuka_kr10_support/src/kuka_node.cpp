a2123db0ec0be4ce9984beee68a7c57b5c7a4be0
cookt <tomcook@mit.edu>
2017-04-04 14:36:14 -0700

    change frame id to view in rviz

--- kuka_kr10_support/src/kuka_node.cpp
+++ kuka_kr10_support/src/kuka_node.cpp
@@ -12,26 +12,30 @@
 static ros::Publisher display_publisher;
 static ros::Publisher marker_pub;
 static visualization_msgs::Marker link_list;
-
+static ros::Rate * r;
+static float f;
 
 void frameCallback(geometry_msgs::PoseArray msg){
   ROS_INFO("frame subscriber callback");
   std::cout << msg << std::endl;
-  link_list.color.r = 1.0;
+  link_list.color.b = 1.0;
   link_list.color.a = 1.0;
-  link_list.scale.x = 0.1; 
+  link_list.scale.x = 1.0;
+  link_list.scale.y = 1.0;
+  link_list.scale.z = 1.0; 
   link_list.header.stamp = ros::Time::now();
-  for (int i  = 0; i < sizeof(msg.poses); i++) {
-    link_list.points.push_back(msg.poses[i].position);
-  }
+  
+  geometry_msgs::Point p;
+  p.x = 1.0;
+  p.y = 1.0;
+  p.z = 0.0;
+  link_list.points.push_back(p);
+  link_list.points.push_back(p);  
   marker_pub.publish(link_list); 
-  sleep(20.0);
+  r->sleep();
 }    
   
  
-
-
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "kuka_node");
@@ -39,16 +43,17 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
   ros::Subscriber frame_sub = node_handle.subscribe("framelinks", 0 , &frameCallback);
-  link_list.header.frame_id = "/my_frame";
+  link_list.header.frame_id = "world";
   link_list.action = visualization_msgs::Marker::ADD;
   link_list.pose.orientation.w = 1.0;
   link_list.id = 0; 
   link_list.type = visualization_msgs::Marker::LINE_LIST;
-  marker_pub = node_handle.advertise<visualization_msgs::Marker>("visualization_marker",10);
+  marker_pub = node_handle.advertise<visualization_msgs::Marker>("visualization_marker",0);
   /* This sleep is ONLY to allow Rviz to come up */
   // We will use the :planning_scene_interface:`PlanningSceneInterface`
   // class to deal directly with the world.
-  sleep(20.0);
+  r = new ros::Rate(20.0);
+  r->sleep();
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface;  
 
   // Publisher for visualizing plans in Rviz.
