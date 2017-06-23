617b8f4599f29eb416dd116fef84568e26d4d626
Yijiang Huang <yijiangh@mit.edu>
2017-06-23 12:19:33 -0700

    gui wip - params load, save and service added. Ready to test.

--- framefab_core/include/framefab_core/framefab_core_service.h
+++ framefab_core/include/framefab_core/framefab_core_service.h
@@ -6,42 +6,9 @@
 
 // msgs
 #include <framefab_msgs/ModelInputParameters.h>
+#include <framefab_msgs/PathInputParameters.h>
 
 // actions
-#include <framefab_msgs/ProcessExecutionAction.h>
-#include <framefab_msgs/ProcessPlanningAction.h>
-#include <framefab_msgs/SimulateMotionPlanAction.h>
-
-#include <actionlib/server/simple_action_server.h>
-#include <actionlib/client/simple_action_client.h>
-
-namespace framfab_core
-{
-struct ProcessPathDetails
-{
-  std::vector <std::vector<geometry_msgs::PoseArray>> blend_poses_;
-  std::vector <geometry_msgs::PoseArray>              edge_poses_;
-  std::vector <std::vector<geometry_msgs::PoseArray>> scan_poses_;
-};
-
-/**
- * Associates a name with a visual msgs marker which contains a pose and sequence of points defining
- * a path
- */
-struct ProcessPathResult
-{
-  typedef std::pair <std::string, std::vector<geometry_msgs::PoseArray>> value_type;
-  std::vector <value_type> paths;
-};
-
-/**
- * Associates a name with a joint trajectory
- */
-struct ProcessPlanResult
-{
-  typedef std::pair <std::string, framefab_msgs::ProcessPlan> value_type;
-  std::vector <value_type> plans;
-};
 
 class FrameFabCoreService
 {
@@ -52,120 +19,34 @@ class FrameFabCoreService
   void run();
 
  private:
-  bool load_blend_parameters(const std::string &filename);
-  void save_blend_parameters(const std::string &filename);
-  bool load_path_planning_parameters(const std::string &filename);
-  void save_path_planning_parameters(const std::string &filename);
-
-  /**
-   * The following path generation and planning methods are defined in
-   * src/blending_service_path_generation.cpp
-   */
-  bool generate_process_plan(framefab_process_path_generation::VisualizeBlendingPlan &process_plan);
-
-  // Service callbacks, these components drive this class by signalling events
-  // from the user
-  bool surface_detection_server_callback(framefab_msgs::SurfaceDetection::Request &req,
-                                         framefab_msgs::SurfaceDetection::Response &res);
-
-  void processPlanningActionCallback(const framefab_msgs::ProcessPlanningGoalConstPtr &goal);
-
-  void simulateMotionPlansActionCallback(const framefab_msgs::SimulateMotionPlanGoalConstPtr &goal_in);
+  bool load_model_input_parameters(const std::string& filename);
+  void save_model_input_parameters(const std::string& filename);
+  bool load_path_input_parameters(const std::string& filename);
+  void save_path_input_parameters(const std::string& filename);
 
   bool
-  framefab_parameters_server_callback(framefab_msgs::FrameFabCoreParameters::Request &req,
-                                           framefab_msgs::FrameFabCoreParameters::Response &res);
-
-  // Reads from the surface selection server and generates blend/scan paths for each
-  framefab_surface_detection::TrajectoryLibrary
-  generateMotionLibrary(const framefab_msgs::PathPlanningParameters &params);
-
-  bool generateProcessPath(const int &id, ProcessPathResult &result);
-
-  bool generateProcessPath(const int &id,
-                           const std::string &name,
-                           const pcl::PolygonMesh &mesh,
-                           const framefab_surface_detection::detection::CloudRGB::Ptr,
-                           ProcessPathResult &result);
-
-  bool generateBlendPath(const framefab_msgs::PathPlanningParameters &params,
-                         const pcl::PolygonMesh &mesh,
-                         std::vector <geometry_msgs::PoseArray> &result);
-
-  bool generateEdgePath(framefab_surface_detection::detection::CloudRGB::Ptr surface,
-                        std::vector <geometry_msgs::PoseArray> &result);
-
-  ProcessPlanResult generateProcessPlan(const std::string &name,
-                                        const std::vector <geometry_msgs::PoseArray> &path,
-                                        const framefab_msgs::BlendingPlanParameters &params,
-                                        const framefab_msgs::ScanPlanParameters &scan_params);
-
-  void visualizePaths();
-
-  void visualizePathPoses();
-
-  void visualizePathStrips();
-
-  std::string getBlendToolPlanningPluginName() const;
-
-  std::string getScanToolPlanningPluginName() const;
+  framefab_parameters_server_callback(framefab_msgs::FrameFabParameters::Request& req,
+                                      framefab_msgs::FrameFabParameters::Response& res);
 
   // Services offered by this class
   ros::ServiceServer framefab_parameters_server_;
 
   // Services subscribed to by this class
-  ros::ServiceClient process_path_client_;
-  ros::ServiceClient trajectory_planner_client_;
-
-  ros::ServiceClient blend_planning_client_;
 
   // Actions offered by this class
-  ros::NodeHandle nh_;
-  actionlib::SimpleActionServer <framefab_msgs::ProcessPlanningAction>    process_planning_server_;
-  actionlib::SimpleActionServer <framefab_msgs::SimulateMotionPlanAction> simulate_motion_plan_server_;
-  framefab_msgs::ProcessPlanningFeedback  process_planning_feedback_;
-  framefab_msgs::ProcessPlanningResult    process_planning_result_;
 
   // Actions subscribed to by this class
-  actionlib::SimpleActionClient <framefab_msgs::ProcessExecutionAction> process_exe_client_;
 
   // Current state publishers
-  ros::Publisher selected_surf_changed_pub_;
-  ros::Publisher tool_path_markers_pub_;
-  ros::Publisher blend_visualization_pub_;
-  ros::Publisher edge_visualization_pub_;
-  ros::Publisher scan_visualization_pub_;
 
-  // Timers
-  bool stop_tool_animation_;
+  // Parameters
+  framefab_msgs::ModelInputParameters 	model_input_params_;
+  framefab_msgs::PathInputParameters 	path_input_params_;
 
-//  // marker server instance
-//  framefab_surface_detection::interactive::InteractiveSurfaceServer surface_server_;
-  // data coordinator
-//  framefab_surface_detection::data::DataCoordinator data_coordinator_;
-
-  // parameters
-  framefab_msgs::ModelInputParameters       default_model_input_plan_params_;
-  framefab_msgs::ProcessPlanningParameters  default_process_planning_params_;
-
-  framefab_msgs::ModelInputParameters       model_input_plan_params_;
-  framefab_msgs::ProcessPlanningParameters  process_planning_params_;
-
-  // results
-  ProcessPathDetails process_path_results_;
-  std::vector <ProcessPlanResult::value_type> process_poses_;
-  std::vector <std::vector<ros::Duration>>    duration_results_;
-  // returned by visualize plan service, needed by trajectory planner
-
-  // parameters
-  bool save_data_;
-  std::string save_location_;
-
-  int marker_counter_;
-
+  framefab_msgs::ModelInputParameters 	default_model_input_params_;
+  framefab_msgs::PathInputParameters 	default_path_input_params_;
   // Parameter loading and saving
   std::string param_cache_prefix_;
 };
-} // namespace
 
 #endif
--- framefab_core/src/framefab_core_service.cpp
+++ framefab_core/src/framefab_core_service.cpp
@@ -1,58 +1,22 @@
 #include <framefab_core/framefab_core_service.h>
 
-// srv
-#include <framefab_msgs/TrajectoryExecution.h>
-
-// Process Planning Srv
-#include <framefab_msgs/FrameFabProcessPlanning.h>
-//#include <framefab_msgs/ProcessPlanning.h>
-
 #include <framefab_param_helpers/framefab_param_helpers.h>
+
+// Process Planning
 
 // topics and services
 const static std::string SAVE_DATA_BOOL_PARAM = "save_data";
 const static std::string SAVE_LOCATION_PARAM = "save_location";
-const static std::string TRAJECTORY_PLANNING_SERVICE = "trajectory_planner";
 const static std::string FRAMEFAB_PARAMETERS_SERVICE = "framefab_parameters";
-const static std::string PROCESS_PATH_SERVICE = "process_path";
-const static std::string PATH_GENERATION_SERVICE = "process_path_generator";
-const static std::string VISUALIZE_BLENDING_PATH_SERVICE = "visualize_path_generator";
-
-const static std::string PROCESS_EXECUTION_SERVICE = "process_execution";
-const static std::string PROCESS_PLANNING_SERVICE = "process_planning";
-
-const static std::string TOOL_PATH_PREVIEW_TOPIC = "tool_path_preview";
-const static std::string EDGE_VISUALIZATION_TOPIC = "edge_visualization";
-const static std::string BLEND_VISUALIZATION_TOPIC = "blend_visualization";
-const static std::string SCAN_VISUALIZATION_TOPIC = "scan_visualization";
-const static std::string ROBOT_SCAN_PATH_PREVIEW_TOPIC = "robot_scan_path_preview";
-
-const static std::string EDGE_IDENTIFIER = "_edge_";
-
-//  tool visual properties
-//const static float TOOL_DIA = .050;
-//const static float TOOL_THK = .005;
-//const static float TOOL_SHAFT_DIA = .006;
-//const static float TOOL_SHAFT_LEN = .045;
-//const static std::string TOOL_FRAME_ID = "process_tool";
 
 // Default filepaths and namespaces for caching stored parameters
-const static std::string MODEL_INPUT_PARAMS_FILE = "framefab_model_input_parameters.msg";
-//const static std::string PATH_PLANNING_PARAMS_FILE = "framefab_path_planning_parameters.msg";
-const static std::string PROCESS_PLANNING_PARAMS_FILE = "framefab_process_planning_parameters.msg";
+const static std::string MODEL_INPUT_PARAMS_FILE = "model_input_parameters.msg";
+const static std::string PATH_INPUT_PARAMS_FILE = "path_input_parameters.msg";
 
 // action server name
-const static std::string PROCESS_EXE_ACTION_SERVER_NAME = "process_execution_as";
-const static std::string PROCESS_PLANNING_ACTION_SERVER_NAME = "process_planning_as";
-const static std::string SIMULATE_MOTION_PLAN_ACTION_SERVER_NAME = "simulate_motion_plan_as";
-const static int PROCESS_EXE_BUFFER = 5;  // Additional time [s] buffer between when blending should end and timeout
 
-FrameFabCoreService::FrameFabCoreService() : save_data_(false),
-  process_exe_client_(PROCESS_EXE_ACTION_SERVER_NAME, true),
-  process_planning_server_(nh_, PROCESS_PLANNING_ACTION_SERVER_NAME,
-                           boost::bind(&FrameFabCoreService::processPlanningActionCallback, this, _1), false),
-  simulate_motion_plan_server_(nh_, SIMULATE_MOTION_PLAN_ACTION_SERVER_NAME,
-                             boost::bind(&FrameFabCoreService::simulateMotionPlansActionCallback, this, _1), false)
+FrameFabCoreService::FrameFabCoreService()
+    : save_data_(false)
 {}
 
 bool FrameFabCoreService::init()
@@ -66,62 +30,20 @@ bool FrameFabCoreService::init()
   // Load the 'prefix' that will be combined with parameters msg base names to save to disk
   ph.param<std::string>("param_cache_prefix", param_cache_prefix_, "");
 
-  if (!this->load_process_planning_parameters(param_cache_prefix_ + PROCESS_PLANNING_PARAMS_FILE))
-    ROS_WARN("Unable to load framefab process parameters.");
-
-  if (!this->load_model_input_parameters(param_cache_prefix_ + MODEL_INPUT_PARAMS_FILE))
-    ROS_WARN("Unable to load framefab process parameters.");
-
-  //TODO: make sure action (sequence) planning component is loaded
-
-  // save default parameters
-  default_model_input_plan_params_ = model_input_plan_params_;
-
-//  if (surface_server_.init() && data_coordinator_.init())
-//  {
-//    // adding callbacks
-//    ROS_INFO_STREAM("Surface detection service initialization succeeded");
-//  }
-//  else
-//  {
-//    ROS_ERROR_STREAM("Surface detection service had an initialization error");
-//  }
-
-  // start server
-//  interactive::InteractiveSurfaceServer::SelectionCallback f =
-//      boost::bind(&FrameFabCoreService::publish_selected_surfaces_changed, this);
-//  surface_server_.add_selection_callback(f);
-
-  // service clients
-//  process_path_client_ = nh_.serviceClient<framefab_msgs::PathPlanning>(PATH_GENERATION_SERVICE);
-
-  // Process Execution Parameters
-  blend_planning_client_ = nh_.serviceClient<framefab_msgs::BlendProcessPlanning>(PROCESS_PLANNING_SERVICE);
-
-  surf_blend_parameters_server_ =
+  // service servers
+  framefab_parameters_server_ =
       nh_.advertiseService(FRAMEFAB_PARAMETERS_SERVICE,
-                          &FrameFabCoreService::framefab_parameters_server_callback, this);
-
-  get_motion_plans_server_ = nh_.advertiseService(
-      SIMULATE_MOTION_PLANS_SERVICE, &FrameFabCoreService::simulateMotionPlansCallback, this);
+                           &FrameFabCoreService::framefab_parameters_server_callback, this);
 
   // publishers
-  tool_path_markers_pub_ = nh_.advertise<visualization_msgs::MarkerArray>(TOOL_PATH_PREVIEW_TOPIC, 1, true);
-  blend_visualization_pub_ = nh_.advertise<geometry_msgs::PoseArray>(BLEND_VISUALIZATION_TOPIC, 1, true);
-  edge_visualization_pub_ = nh_.advertise<geometry_msgs::PoseArray>(EDGE_VISUALIZATION_TOPIC, 1, true);
-  scan_visualization_pub_ = nh_.advertise<geometry_msgs::PoseArray>(SCAN_VISUALIZATION_TOPIC, 1, true);
 
   // action servers
-  process_planning_server_.start();
-  simulate_motion_plan_server_.start();
 
   return true;
 }
 
 void FrameFabCoreService::run()
 {
-  surface_server_.run();
-
   ros::Duration loop_duration(1.0f);
   while (ros::ok())
   {
@@ -129,38 +51,10 @@ void FrameFabCoreService::run()
   }
 }
 
-// Blending Parameters
-bool FrameFabCoreService::load_model_input_parameters(const std::string& filename)
+
+bool FrameFabCoreService::load_model_input_parameters(const std::string & filename)
 {
-  using framefab_param_helpers::loadParam;
-  using framefab_param_helpers::loadBoolParam;
-
-  if (framefab_param_helpers::fromFile(filename, model_input_plan_params_))
-  {
-    return true;
-  }
-  // otherwise default to the parameter server
-  ros::NodeHandle nh("~/model_input");
-  return loadParam(nh, "ref_pt_x", blending_plan_params_.ref_pt_x) &&
-         loadParam(nh, "ref_pt_y", blending_plan_params_.ref_pt_x) &&
-         loadParam(nh, "ref_pt_z", blending_plan_params_.ref_pt_x) &&
-         loadParam(nh, "unit_type", blending_plan_params_.unit_type) &&
-         loadParam(nh, "file_name", blending_plan_params_.file_name);
-}
-
-
-void FrameFabCoreService::save_model_input_parameters(const std::string& filename)
-{
-  if (!framefab_param_helpers::toFile(filename, model_input_plan_params_))
-  {
-    ROS_WARN_STREAM("Unable to save model input parameters to: " << filename);
-  }
-}
-
-
-bool FrameFabCoreService::load_process_planning_parameters(const std::string & filename)
-{
-  if(framefab_param_helpers::fromFile(filename, path_planning_params_))
+  if(framefab_param_helpers::fromFile(filename, model_input_params_))
   {
     return true;
   }
@@ -168,248 +62,62 @@ bool FrameFabCoreService::load_process_planning_parameters(const std::string & f
 }
 
 
-void FrameFabCoreService::save_process_planning_parameters(const std::string & filename)
+void FrameFabCoreService::save_model_input_parameters(const std::string& filename)
 {
-  if(!framefab_param_helpers::toFile(filename, path_planning_params_))
+  if (!framefab_param_helpers::toFile(filename, model_input_params_))
   {
-    ROS_WARN_STREAM("Unable to save path-planning parameters to: " << filename);
+    ROS_WARN_STREAM("Unable to save model input parameters to: " << filename);
   }
 }
 
-void FrameFabCoreService::processPlanningActionCallback(const framefab_msgs::ProcessPlanningGoalConstPtr &goal_in)
+bool FrameFabCoreService::load_path_input_parameters(const std::string & filename)
 {
-  switch (goal_in->action)
+  if(framefab_param_helpers::fromFile(filename, path_input_params_))
   {
-    case framefab_msgs::ProcessPlanningGoal::GENERATE_MOTION_PLAN_AND_PREVIEW:
-    {
-      process_planning_feedback_.last_completed = "Recieved request to generate motion plan";
-      process_planning_server_.publishFeedback(process_planning_feedback_);
-      process_planning_feedback_.last_completed = "Finished planning. Visualizing...";
-      process_planning_server_.publishFeedback(process_planning_feedback_);
-      visualizePaths();
-      process_planning_result_.succeeded = true;
-      process_planning_server_.setSucceeded(process_planning_result_);
-      break;
-    }
-    case framefab_msgs::ProcessPlanningGoal::PREVIEW_TOOL_PATH:
-    {
-      process_planning_feedback_.last_completed = "Recieved request to preview tool path";
-      process_planning_server_.publishFeedback(process_planning_feedback_);
-      break;
-    }
-    default:
-    {
-      ROS_ERROR_STREAM("Unknown action code '" << goal_in->action << "' request");
-      break;
-    }
+    return true;
   }
-
-  process_planning_result_.succeeded = false;
+  return false;
 }
 
-bool FrameFabCoreService::framefab_parameters_server_callback(
-    framefab_msgs::FrameFabCoreParameters::Request& req,
-    framefab_msgs::FrameFabCoreParameters::Response& res)
+void FrameFabCoreService::save_path_input_parameters(const std::string & filename)
+{
+  if(!framefab_param_helpers::toFile(filename, path_input_params_))
+  {
+    ROS_WARN_STREAM("Unable to save path input parameters to: " << filename);
+  }
+}
+
+bool FrameFabService::framefab_parameters_server_callback(
+    framefab_msgs::FrameFabParameters::Request& req,
+    framefab_msgs::FrameFabParameters::Response& res)
 {
   switch (req.action)
   {
-  case framefab_msgs::FrameFabCoreParameters::Request::GET_CURRENT_PARAMETERS:
+    case framefab_msgs::FrameFabParameters::Request::GET_CURRENT_PARAMETERS:
+      res.model_params = model_input_params_;
+      res.path_params  = path_input_params_;
+      break;
 
-    res.model_input_params  = model_input_params_;
-    res.process_plan_params = process_plan_params_;
-    break;
+    case framefab_msgs::FrameFabParameters::Request::GET_DEFAULT_PARAMETERS:
+      res.model_params = default_model_input_params_;
+      res.path_params  = default_path_input_params_;
+      break;
 
-  case framefab_msgs::FrameFabCoreParameters::Request::GET_DEFAULT_PARAMETERS:
+      // Update the current parameters in this service
+    case framefab_msgs::FrameFabParameters::Request::SET_PARAMETERS:
+    case framefab_msgs::FrameFabParameters::Request::SAVE_PARAMETERS:
+      model_input_params_ = req.model_params;
+      path_input_params_ = req.input_params;
 
-    res.model_input_params  = default_model_input_params_;
-    res.process_plan_params = default_process_plan_params_;
-    break;
-
-  // Update the current parameters in this service
-  case framefab_msgs::FrameFabCoreParameters::Request::SET_PARAMETERS:
-  case framefab_msgs::FrameFabCoreParameters::Request::SAVE_PARAMETERS:
-    model_input_params_       = req.model_input_params_;
-    process_planning_params_  = req.process_plan_params;
-
-    if (req.action == framefab_msgs::FrameFabCoreParameters::Request::SAVE_PARAMETERS)
-    {
-      this->save_model_input_parameters(param_cache_prefix_ + MODEL_INPUT_PARAMS_FILE);
-      this->save_process_planning_parameters(param_cache_prefix_ + PROCESS_PLANNING_PARAMS_FILE);
-    }
-    break;
+      if (req.action == framefab_msgs::FrameFabParameters::Request::SAVE_PARAMETERS)
+      {
+        this->save_model_input_parameters(param_cache_prefix_ + MODEL_INPUT_PARAMS_FILE);
+        this->save_path_input_parameters(param_cache_prefix_ + PATH_INPUT_PARAMS_FILE);
+      }
+      break;
   }
 
   return true;
-}
-
-void FrameFabCoreService::simulateMotionPlansActionCallback(const framefab_msgs::SimulateMotionPlanGoalConstPtr& goal_in)
-{
-  framefab_msgs::SimulateMotionPlanResult res;
-
-//  // If plan does not exist, abort and return
-//  if (trajectory_library_.get().find(goal_in->name) == trajectory_library_.get().end())
-//  {
-//    ROS_WARN_STREAM("Motion plan " << goal_in->name << " does not exist. Cannot execute.");
-//    res.code = framefab_msgs::SimulateMotionPlanResponse::NO_SUCH_NAME;
-//    simulate_motion_plan_server_.setAborted(res);
-//    return;
-//  }
-
-  bool is_blend = trajectory_library_.get()[goal_in->name].type == framefab_msgs::ProcessPlan::BLEND_TYPE;
-
-  // Send command to execution server
-  framefab_msgs::ProcessExecutionActionGoal goal;
-  goal.goal.trajectory_approach = trajectory_library_.get()[goal_in->name].trajectory_approach;
-  goal.goal.trajectory_depart = trajectory_library_.get()[goal_in->name].trajectory_depart;
-  goal.goal.trajectory_process = trajectory_library_.get()[goal_in->name].trajectory_process;
-  goal.goal.wait_for_execution = goal_in->wait_for_execution;
-  goal.goal.simulate = goal_in->simulate;
-
-  actionlib::SimpleActionClient<framefab_msgs::ProcessExecutionAction> *exe_client = process_exe_client_;
-  exe_client->sendGoal(goal.goal);
-
-  ros::Duration process_time(goal.goal.trajectory_depart.points.back().time_from_start);
-  ros::Duration buffer_time(PROCESS_EXE_BUFFER);
-  if(exe_client->waitForResult(process_time + buffer_time))
-  {
-    res.code = framefab_msgs::SimulateMotionPlanResult::SUCCESS;
-    simulate_motion_plan_server_.setSucceeded(res);
-  }
-  else
-  {
-    res.code=framefab_msgs::simulateMotionPlanResult::TIMEOUT;
-    simulate_motion_plan_server_.setAborted(res);
-  }
-}
-
-void FrameFabCoreService::visualizePaths()
-{
-  visualizePathPoses();
-
-  visualizePathStrips();
-}
-
-void FrameFabCoreservice::visualizepathposes()
-{
-  // publish poses
-  geometry_msgs::posearray blend_poses, edge_poses, scan_poses;
-  blend_poses.header.frame_id = edge_poses.header.frame_id = scan_poses.header.frame_id = "world_frame";
-  blend_poses.header.stamp = edge_poses.header.stamp = scan_poses.header.stamp = ros::time::now();
-
-  for (const auto& path : process_path_results_.blend_poses_)
-  {
-    for (const auto& pose_array : path)
-    {
-      blend_poses.poses.insert(blend_poses.poses.end(), pose_array.poses.begin(), pose_array.poses.end());
-    }
-  }
-
-  for(const auto& pose_array : process_path_results_.edge_poses_)
-    edge_poses.poses.insert(edge_poses.poses.end(), pose_array.poses.begin(), pose_array.poses.end());
-
-  for (const auto& path : process_path_results_.scan_poses_)
-  {
-    for(const auto& pose_array : path)
-    {
-      scan_poses.poses.insert(scan_poses.poses.end(), pose_array.poses.begin(), pose_array.poses.end());
-    }
-  }
-
-  blend_visualization_pub_.publish(blend_poses);
-  edge_visualization_pub_.publish(edge_poses);
-  scan_visualization_pub_.publish(scan_poses);
-}
-
-static visualization_msgs::Marker makeLineStripMarker(const std::string& ns, const int id, const std_msgs::ColorRGBA& color,
-                                                      const geometry_msgs::PoseArray& segment)
-{
-  visualization_msgs::Marker marker;
-  marker.header.frame_id = "world_frame";
-  marker.header.stamp = ros::Time::now();
-  marker.id = id;
-  marker.ns = ns;
-  marker.pose.orientation.w = 1;
-  marker.action = visualization_msgs::Marker::ADD;
-  marker.type = visualization_msgs::Marker::LINE_STRIP;
-  marker.scale.x = 0.004;
-  marker.color = color;
-  marker.lifetime = ros::Duration(0.0);
-
-  for (const auto& pose : segment.poses)
-    marker.points.push_back(pose.position);
-
-  return marker;
-}
-
-void FrameFabCoreService::visualizePathStrips()
-{
-  visualization_msgs::MarkerArray path_visualization;
-
-  // Visualize Blending Paths
-  std_msgs::ColorRGBA blend_color;
-  blend_color.r = 1.0;
-  blend_color.b = 0.0;
-  blend_color.g = 0.0;
-  blend_color.a = 0.8;
-
-  const std::string blend_ns = "blend_paths";
-
-  int blend_path_id = 0;
-
-  for (const auto& path : process_path_results_.blend_poses_) // for a given surface
-  {
-    for (const auto& segment : path) // for a given path segment on the surface
-    {
-      path_visualization.markers.push_back(makeLineStripMarker(blend_ns, blend_path_id++, blend_color, segment));
-    }
-  }
-
-  // Visualize scan paths
-  std_msgs::ColorRGBA scan_color;
-  scan_color.r = 1.0;
-  scan_color.b = 0.0;
-  scan_color.g = 1.0;
-  scan_color.a = 0.8;
-
-  const std::string scan_ns = "scan_paths";
-
-  int scan_path_id = 0;
-
-  for (const auto& path : process_path_results_.scan_poses_) // for a given surface
-  {
-    for (const auto& segment : path) // for a given path segment on the surface
-    {
-      path_visualization.markers.push_back(makeLineStripMarker(scan_ns, scan_path_id++, scan_color, segment));
-    }
-  }
-
-  tool_path_markers_pub_.publish(path_visualization);
-}
-
-std::string FrameFabCoreService::getBlendToolPlanningPluginName() const
-{
-  ros::NodeHandle pnh ("~");
-  std::string name;
-  if (!pnh.getParam(BLEND_TOOL_PLUGIN_PARAM, name))
-  {
-    ROS_WARN("Unable to load blend tool planning plugin from ros param '%s'",
-             BLEND_TOOL_PLUGIN_PARAM.c_str());
-  }
-
-  return name;
-}
-
-std::string FrameFabCoreService::getScanToolPlanningPluginName() const
-{
-  ros::NodeHandle pnh ("~");
-  std::string name;
-  if (!pnh.getParam(SCAN_TOOL_PLUGIN_PARAM, name))
-  {
-    ROS_WARN("Unable to load scan tool planning plugin from ros param '%s'",
-             SCAN_TOOL_PLUGIN_PARAM.c_str());
-  }
-
-  return name;
 }
 
 int main(int argc, char** argv)
