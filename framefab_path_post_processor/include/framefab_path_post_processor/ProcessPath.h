631c248225e716e47740e2b9271912e77e5d4690
Yijiang Huang <yijiangh@mit.edu>
2017-06-26 07:52:06 -0700

    ProcessPostProcessor wip - ProcessPath structure added.

---
+++ framefab_path_post_processor/include/framefab_path_post_processor/ProcessPath.h
@@ -0,0 +1,81 @@
+//
+// Created by yijiangh on 6/26/17.
+//
+
+#ifndef FRAMEFAB_PATH_POST_PROCESSOR_PROCESSPATH_H
+#define FRAMEFAB_PATH_POST_PROCESSOR_PROCESSPATH_H
+
+#include <framefab_msgs/ElementCandidatePoses>
+#include <Eigen/Core>
+
+namespace framefab_utils
+{
+
+class UnitProcessPath
+{
+ public:
+  UnitProcessPath(){};
+  virtual UnitProcessPath(){};
+
+  void createUnitProcessPath(Eigen::Vector3d st_pt, Eigen::Vector3d end_pt,
+                             std::vector<Eigen::Vector3d> feasible_orients,
+                             std::string type_str,
+                             double shrink_length = 0.01)
+  {
+    st_pt_ = st_pt;
+    end_pt_ = end_pt;
+    feasible_orients_ = feasible_orients;
+    type_ = type_str;
+    shrink_length_ = shrink_length;
+  }
+
+  Eigen::Vector3d& getStartPt() const { return st_pt_; }
+  Eigen::Vector3d& getEndPt() const { return end_pt_; }
+  std::vector<Eigen::Vector3d>& getFeasibleOrients() const { return feasible_orients_; }
+  std::string& getType() const { return type_; }
+
+  framefab_msgs::ElementCandidatePoses asElementCandidatePoses() const;
+
+ protected:
+  void createCollisionObject();
+
+ private:
+  // st pt, end pt, feasible orients, type
+  Eigen::Vector3d st_pt_;
+  Eigen::Vector3d st_pt_;
+  std::vector<Eigen::Vector3d> feasible_orients_;
+  std::string type_;
+
+  // collision objects
+  double shrink_length_; // meters
+
+};
+
+class ProcessPath
+{
+  typedef std::vector<ElementCandidatePoses> ElementCandidatePosesArray;
+
+ public:
+  ProcessPath(){};
+  virtual ~ProcessPath(){};
+
+  void addUnitProcessPath(const UnitProcessPath& u_pth) { process_paths_.push_back(u_pth); }
+
+  std::vector<UnitProcessPath> data() const
+  {
+    return process_paths_;
+  }
+
+  void clear()
+  {
+    process_paths_.clear();
+  }
+
+  ElementCandidatePosesArray asElementCandidatePosesArray() const;
+
+private:
+  std::vector<UnitProcessPath> process_paths_;
+};
+}
+
+#endif //FRAMEFAB_PATH_POST_PROCESSOR_PROCESSPATH_H
--- framefab_path_post_processor/include/framefab_path_post_processor/path_post_processor.h
+++ framefab_path_post_processor/include/framefab_path_post_processor/path_post_processor.h
@@ -5,6 +5,27 @@
 #ifndef FRAMEFAB_PATH_POST_PROCESSOR_PATH_POST_PROCESSOR_H
 #define FRAMEFAB_PATH_POST_PROCESSOR_PATH_POST_PROCESSOR_H
 
+namespace framefab_path_post_processing
+{
 
+class PathPostProcessor
+{
+
+ public:
+  PathPostProcessor();
+  virtual ~PathPostProcessor() {};
+
+  bool createCandidatePoses();
+  const ElementCandidatePoses& getCandidatePoses() const { return candidate_poses; }
+
+  // data setting
+  void setPathInputParams(framefab_msgs::PathInputParameters);
+
+ private:
+  framefab_msgs::PathInputParameters path_input_params_;
+
+};
+
+}
 
 #endif //FRAMEFAB_PATH_POST_PROCESSOR_PATH_POST_PROCESSOR_H
