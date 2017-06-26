9b79895ff19769cf872dcc1a3e14fa315b69fa87
Yijiang Huang <yijiangh@mit.edu>
2017-06-26 15:32:04 -0700

    PathPostProcessor wip - fetch back data from Json file succeeded.

--- framefab_path_post_processor/src/path_post_processor.cpp
+++ framefab_path_post_processor/src/path_post_processor.cpp
@@ -32,13 +32,26 @@ bool framefab_path_post_processing::PathPostProcessor::createCandidatePoses()
   fclose(fp);
   ROS_INFO_STREAM("path json Parsing succeeded.");
 
-  /* --- 2. Access values in the document, create processPath out of them --- */
-//  static const char* kTypeNames[] = { "Null", "False", "True", "Object", "Array", "String", "Number" };
-  for (Value::ConstMemberIterator itr = document.MemberBegin(); itr != document.MemberEnd(); ++itr)
+  int m = document["element_number"].GetInt();
+  ROS_INFO_STREAM("model element member: " << m);
+
+  const Value& process_path_array = document["sequenced_elements"];
+  assert(process_path_array.IsArray());
+
+  for (SizeType i = 0; i < process_path_array.Size(); i++)
   {
-    ROS_INFO_STREAM("Json member " << itr->name.GetString());
+    const Value& element_path = process_path_array[i];
+    double st_x = element_path["start_pt"][0].GetDouble();
+    double st_y = element_path["start_pt"][1].GetDouble();
+    double st_z = element_path["start_pt"][2].GetDouble();
+
+    double end_x = element_path["end_pt"][0].GetDouble();
+    double end_y = element_path["end_pt"][1].GetDouble();
+    double end_z = element_path["end_pt"][2].GetDouble();
+
+
+//    ROS_INFO_STREAM("Node " << i << " - start pt (" << x << ", " << y << ", " << z << ")");
   }
-//    printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
 
   return true;
 }
