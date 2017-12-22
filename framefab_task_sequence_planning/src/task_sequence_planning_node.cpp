ec276e47cfed627153570ba29e70b889b85c7410
Yijiang Huang <yijiangh@mit.edu>
2017-12-22 10:20:09 -0500

    task seq planner init.

--- framefab_task_sequence_planning/src/task_sequence_planning_node.cpp
+++ framefab_task_sequence_planning/src/task_sequence_planning_node.cpp
@@ -2,3 +2,29 @@
 // Created by yijiangh on 12/21/17.
 //
 
+#include <ros/ros.h>
+#include <framefab_task_sequence_planning/framefab_task_sequence_planning.h>
+
+// Globals
+const static std::string DEFAULT_TASK_SEQUENCE_PLANNING_SERVICE = "task_sequence_planning";
+
+bool planTaskSequenceCallback(framefab_msgs::TaskSequencePlanningRequest& req,
+                              framefab_msgs::TaskSequencePlanningResponse& res)
+{
+  return true;
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "framefab_task_sequence_planning");
+
+  ros::ServiceServer task_sequence_processing_server =
+      nh.advertiseService<framefab_msgs::TaskSequenceProcessingRequest, framefab_msgs::TaskSequenceProcessingResponse>(
+          "task_sequence_processing", boost::bind(processTaskSequenceCallback, _1, _2));
+
+  // Serve and wait for shutdown
+  ROS_INFO_STREAM("[tsp] sequence task planning server online");
+  ros::spin();
+
+  return 0;
+}
